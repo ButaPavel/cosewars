229ef8c84c0970ee125171a75d8eae838165bfc7
ButaPavel <buta.pavel84@gmail.com>
2022-11-19 13:49:31 +0300

    2

--- 1.js
+++ 1.js
@@ -1,2 +1,11 @@
-var
-1
+//function invert(array) {
+    //arr.sort( (a, b) => a - b );
+array = [1, 2 ,3]
+ ar = array.map( (item) => (-item) )
+
+
+    /*  var arrLength = array.map( function(item) {
+    return -item;
+  });*/
+   console.log(ar) ;
+//}
