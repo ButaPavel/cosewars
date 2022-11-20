a472021453e6856ac72be97c7b60b43a8a2c9843
ButaPavel <buta.pavel84@gmail.com>
2022-11-20 22:12:53 +0300

    6

---
+++ 6.js
@@ -0,0 +1,20 @@
+let val = 125
+let origin = (n) => {
+    return (n.toString().split(""))
+}
+for (i = 0; origin(number).length; i++){
+   do {
+
+   }while ()
+
+    if (origin(number)[i] != origin(number)[origin(number).length-1-i]){
+        return
+    }
+}
+console.log(invers(val))
+console.log(origin(val))
+
+/*do {
+
+
+} while()*/
